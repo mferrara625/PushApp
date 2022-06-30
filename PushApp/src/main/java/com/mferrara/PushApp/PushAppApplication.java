bab82379b22fb3f119f7a3b2070b45453b9248fd
mferrara625 <mferrara401@yahoo.com>
2022-06-30 14:53:08 -0400

    initial commit

---
+++ PushApp/src/main/java/com/mferrara/PushApp/PushAppApplication.java
@@ -0,0 +1,13 @@
+package com.mferrara.PushApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushAppApplication.class, args);
+	}
+
+}
---
+++ PushApp/src/test/java/com/mferrara/PushApp/PushAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mferrara.PushApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
