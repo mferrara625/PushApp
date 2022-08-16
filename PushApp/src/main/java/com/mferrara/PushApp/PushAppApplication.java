1dfa14b50c38fa196c046a470c374c3e7cbf8922
mferrara625 <mferrara401@yahoo.com>
2022-08-16 15:34:00 -0400

    added extension to application class

--- PushApp/src/main/java/com/mferrara/PushApp/PushAppApplication.java
+++ PushApp/src/main/java/com/mferrara/PushApp/PushAppApplication.java
@@ -2,9 +2,10 @@ package com.mferrara.PushApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PushAppApplication {
+public class PushAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PushAppApplication.class, args);
